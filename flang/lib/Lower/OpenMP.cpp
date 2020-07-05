5e9e085460fcf38f63483479dd110fbde54830b1
Sourabh Singh Tomar <SourabhSingh.Tomar@amd.com>
2020-07-05 22:59:01 +0530

    [flang][OpenMP] Added support for lowering OpenMP barrier construct
    This patch adds lowering support for OpenMP barrier construct to
    OpenMP Dialect operations.
    
    In order to support lowering, this patch registers OpenMPDialect
    and also adds OpenMPDialect to the legalizer.

--- flang/lib/Lower/OpenMP.cpp
+++ flang/lib/Lower/OpenMP.cpp
@@ -8,15 +8,43 @@
 
 #include "flang/Lower/OpenMP.h"
 #include "flang/Lower/Bridge.h"
+#include "flang/Lower/FIRBuilder.h"
 #include "flang/Lower/PFTBuilder.h"
 #include "flang/Parser/parse-tree.h"
+#include "mlir/Dialect/OpenMP/OpenMPDialect.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/Module.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/MD5.h"
 
 #define TODO() llvm_unreachable("not yet implemented")
 
 void Fortran::lower::genOpenMPConstruct(
-    Fortran::lower::AbstractConverter &, Fortran::lower::pft::Evaluation &,
-    const Fortran::parser::OpenMPConstruct &) {
-  TODO();
+    Fortran::lower::AbstractConverter &ABSConv,
+    Fortran::lower::pft::Evaluation &Eval,
+    const Fortran::parser::OpenMPConstruct &OMPConstruct) {
+  if (auto StandaloneConstruct =
+          std::get_if<Fortran::parser::OpenMPStandaloneConstruct>(
+              &OMPConstruct.u)) {
+
+    if (auto SimpleStandaloneConstruct =
+            std::get_if<Fortran::parser::OpenMPSimpleStandaloneConstruct>(
+                &StandaloneConstruct->u)) {
+      const auto &Directive{
+          std::get<Fortran::parser::OmpSimpleStandaloneDirective>(
+              SimpleStandaloneConstruct->t)};
+      switch (Directive.v) {
+      default:
+        TODO();
+      case parser::OmpSimpleStandaloneDirective::Directive::Barrier: {
+        ABSConv.getFirOpBuilder().create<mlir::omp::BarrierOp>(
+            ABSConv.getCurrentLocation());
+        break;
+      }
+      }
+    }
+  }
 }
 
 void Fortran::lower::genOpenMPEndLoop(
