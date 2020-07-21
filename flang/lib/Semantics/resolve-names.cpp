2d69098abd0e6682f5ea0c78ca7ddc95774562ec
vdonaldson <37090318+vdonaldson@users.noreply.github.com>
2020-07-21 12:16:33 -0700

    Implement subprograms with alternate entry points. (#282)
    * Implement subprograms with alternate entry points.
    
    The primary subprogram and each secondary entry point has an independent clone.
    
    * review comment update

--- flang/lib/Semantics/resolve-names.cpp
+++ flang/lib/Semantics/resolve-names.cpp
@@ -2985,6 +2985,7 @@ void SubprogramVisitor::Post(const parser::EntryStmt &stmt) {
       } else {
         dummy = &MakeSymbol(*dummyName, EntityDetails(true));
       }
+      ApplyImplicitRules(*dummy);
       entryDetails.add_dummyArg(*dummy);
     } else {
       if (inFunction) { // C1573
