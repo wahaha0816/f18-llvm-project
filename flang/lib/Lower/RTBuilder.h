3df3e07acb24379785e843d7469b4dbc137007fb
peter klausler <pklausler@nvidia.com>
2020-07-23 11:53:17 -0700

    Call InputComplex{32,64} for formatted COMPLEX input
    These are new runtime I/O APIs that, as it turns out, need
    to exist so that list-directed I/O can distinguish
    COMPLEX from REAL when dealing with null inputs.

--- flang/lib/Lower/RTBuilder.h
+++ flang/lib/Lower/RTBuilder.h
@@ -138,6 +138,13 @@ constexpr TypeBuilderFunc getModel<double &>() {
   };
 }
 template <>
+constexpr TypeBuilderFunc getModel<double *>() {
+  return [](mlir::MLIRContext *context) -> mlir::Type {
+    TypeBuilderFunc f{getModel<double>()};
+    return fir::PointerType::get(f(context));
+  };
+}
+template <>
 constexpr TypeBuilderFunc getModel<float>() {
   return [](mlir::MLIRContext *context) -> mlir::Type {
     return mlir::FloatType::getF32(context);
@@ -148,6 +155,13 @@ constexpr TypeBuilderFunc getModel<float &>() {
   return [](mlir::MLIRContext *context) -> mlir::Type {
     TypeBuilderFunc f{getModel<float>()};
     return fir::ReferenceType::get(f(context));
+  };
+}
+template <>
+constexpr TypeBuilderFunc getModel<float *>() {
+  return [](mlir::MLIRContext *context) -> mlir::Type {
+    TypeBuilderFunc f{getModel<float>()};
+    return fir::PointerType::get(f(context));
   };
 }
 template <>
