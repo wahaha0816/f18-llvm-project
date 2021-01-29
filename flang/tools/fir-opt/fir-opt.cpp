d8026c88eb4fd31d06acca9c8b35f15cb618cce8
Valentin Clement <clementval@gmail.com>
2021-01-29 10:41:11 -0500

    [flang] Add fir-opt tool

---
+++ flang/tools/fir-opt/fir-opt.cpp
@@ -0,0 +1,28 @@
+//===- fir-opt.cpp - FIR Optimizer Driver -----------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This is to be like LLVM's opt program, only for FIR.  Such a program is
+// required for roundtrip testing, etc.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Support/MlirOptMain.h"
+#include "flang/Optimizer/Dialect/FIRDialect.h"
+#include "flang/Optimizer/OptPasses.h"
+
+using namespace mlir;
+
+int main(int argc, char **argv) {
+  fir::registerFIRPasses();
+  fir::registerOptPasses();
+  DialectRegistry registry;
+  registerAllDialects(registry);
+  registry.insert<fir::FIROpsDialect>();
+  return failed(MlirOptMain(argc, argv, "FIR modular optimizer driver\n",
+      registry, /*preloadDialectsInContext*/false));
+}
