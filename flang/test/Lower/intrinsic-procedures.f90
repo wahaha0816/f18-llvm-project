d593a78bb8b7939c0846568f4f953cded0b353e5
vdonaldson <37090318+vdonaldson@users.noreply.github.com>
2021-07-01 12:36:35 -0700

    cpu_time intrinsic (#904)

--- flang/test/Lower/intrinsic-procedures.f90
+++ flang/test/Lower/intrinsic-procedures.f90
@@ -169,6 +169,16 @@ function btest_test(i, j)
   btest_test = btest(i, j)
 end
 
+! CPU_TIME
+! CHECK-LABEL: cpu_time_test
+subroutine cpu_time_test(t)
+  real :: t
+  ! CHECK: %[[result64:[0-9]+]] = fir.call @_FortranACpuTime() : () -> f64
+  ! CHECK: %[[result32:[0-9]+]] = fir.convert %[[result64]] : (f64) -> f32
+  ! CHECK: fir.store %[[result32]] to %arg0 : !fir.ref<f32>
+  call cpu_time(t)
+end subroutine
+
 ! DBLE
 ! CHECK-LABEL: dble_test
 subroutine dble_test(a)
