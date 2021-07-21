00c8a2a32db9c3651b6c102793c852b7e895d9e6
Valentin Clement <clementval@gmail.com>
2021-07-21 10:45:09 -0400

    [openacc] Fix lowering tests that violate the attach pointer/allocatable restriction

--- flang/test/Lower/OpenACC/acc-data.f90
+++ flang/test/Lower/OpenACC/acc-data.f90
@@ -2,17 +2,16 @@
 
 ! RUN: bbc -fopenacc -emit-fir %s -o - | FileCheck %s
 
-! FIXME: Test is violating semantic constraint: "Argument on the ATTACH
-! clause must be a variable or array with the POINTER or ALLOCATABLE attribute"
-! XFAIL: true
-
 subroutine acc_data
   real, dimension(10, 10) :: a, b, c
+  real, pointer :: d, e
   logical :: ifCondition = .TRUE.
 
 !CHECK: [[A:%.*]] = fir.alloca !fir.array<10x10xf32> {{{.*}}uniq_name = "{{.*}}Ea"}
 !CHECK: [[B:%.*]] = fir.alloca !fir.array<10x10xf32> {{{.*}}uniq_name = "{{.*}}Eb"}
 !CHECK: [[C:%.*]] = fir.alloca !fir.array<10x10xf32> {{{.*}}uniq_name = "{{.*}}Ec"}
+!CHECK: [[D:%.*]] = fir.alloca !fir.box<!fir.ptr<f32>> {bindc_name = "d", uniq_name = "{{.*}}Ed"}
+!CHECK: [[E:%.*]] = fir.alloca !fir.box<!fir.ptr<f32>> {bindc_name = "e", uniq_name = "{{.*}}Ee"}
 
   !$acc data if(.TRUE.) copy(a)
   !$acc end data
@@ -87,10 +86,10 @@ subroutine acc_data
 !CHECK:        acc.terminator
 !CHECK-NEXT: }{{$}}
 
-  !$acc data attach(b, c)
+  !$acc data attach(d, e)
   !$acc end data
 
-!CHECK:      acc.data attach([[B]], [[C]] : !fir.ref<!fir.array<10x10xf32>>, !fir.ref<!fir.array<10x10xf32>>) {
+!CHECK:      acc.data attach([[D]], [[E]] : !fir.ref<!fir.box<!fir.ptr<f32>>>, !fir.ref<!fir.box<!fir.ptr<f32>>>) {
 !CHECK:        acc.terminator
 !CHECK-NEXT: }{{$}}
 
