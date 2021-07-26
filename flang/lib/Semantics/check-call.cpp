6f129d1c9a6b8c95e0886f65dc8633c59a0806c7
Peter Steinfeld <psteinfeld@nvidia.com>
2021-07-26 14:28:36 -0700

    [flang] Disallow BOZ literal constants as arguments of implicit interfaces
    Since BOZ literal arguments are typeless, we cannot know how to pass them as
    actual arguments to procedures with implicit interfaces.  This change avoids
    the problem by emitting an error message in such situations.
    
    This change stemmed from the following issue --
      https://github.com/flang-compiler/f18-llvm-project/issues/794
    
    Differential Revision: https://reviews.llvm.org/D106831

--- flang/lib/Semantics/check-call.cpp
+++ flang/lib/Semantics/check-call.cpp
@@ -46,7 +46,7 @@ static void CheckImplicitInterfaceArg(
     }
   }
   if (const auto *expr{arg.UnwrapExpr()}) {
-    if (IsBOZLiteral(*expr)) {
+    if (std::holds_alternative<evaluate::BOZLiteralConstant>(expr->u)) {
       messages.Say("BOZ argument requires an explicit interface"_err_en_US);
     }
     if (auto named{evaluate::ExtractNamedEntity(*expr)}) {
