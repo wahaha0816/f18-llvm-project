1f32bddd6032dc55d3f2e81357f4bbf0649d97e0
Valentin Clement (バレンタイン クレメン) <clementval@gmail.com>
2021-09-15 17:06:19 +0200

    [NFC] Unify headers in lib/Lower and lib/Optimizer (#1062)

--- flang/lib/Optimizer/Transforms/FirLoopResultOpt.cpp
+++ flang/lib/Optimizer/Transforms/FirLoopResultOpt.cpp
@@ -1,4 +1,4 @@
-//===- FirLoopResultOpt.cpp - Optimization pass for fir loops    ------ -*-===//
+//===- FirLoopResultOpt.cpp - Optimization pass for fir loops -------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
