57c30de62bca3587ab9c9be698fff637bdaa00f0
Valentin Clement (バレンタイン クレメン) <clementval@gmail.com>
2021-09-15 17:06:19 +0200

    [NFC] Unify headers in lib/Lower and lib/Optimizer (#1062)

--- flang/lib/Optimizer/Transforms/AbstractResult.cpp
+++ flang/lib/Optimizer/Transforms/AbstractResult.cpp
@@ -1,4 +1,4 @@
-//===- AbstractResult.cpp - Conversion of Abstract Function Result    ---*-===//
+//===- AbstractResult.cpp - Conversion of Abstract Function Result --------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
