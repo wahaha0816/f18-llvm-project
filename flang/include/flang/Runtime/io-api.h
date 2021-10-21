aae86b70f11d2e2b803392711958d533d300f2af
Jean Perier <jperier@nvidia.com>
2021-10-21 08:29:25 -0700

    [rebase fix] keep llvm runtime headers as is

--- flang/include/flang/Runtime/io-api.h
+++ flang/include/flang/Runtime/io-api.h
@@ -28,9 +28,6 @@ using Cookie = IoStatementState *;
 using ExternalUnit = int;
 using AsynchronousId = int;
 static constexpr ExternalUnit DefaultUnit{-1}; // READ(*), WRITE(*), PRINT
-} // namespace Fortran::runtime::io
-
-namespace Fortran::runtime::io {
 
 // INQUIRE specifiers are encoded as simple base-26 packings of
 // the spellings of their keywords.
@@ -319,5 +316,4 @@ enum Iostat IONAME(EndIoStatement)(Cookie);
 
 } // extern "C"
 } // namespace Fortran::runtime::io
-
 #endif
