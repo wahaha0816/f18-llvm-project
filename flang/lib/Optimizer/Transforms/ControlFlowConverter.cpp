18f77b023383fe09430f5234e4f7ef23d5514d2e
Jean Perier <jperier@nvidia.com>
2021-10-25 02:06:42 -0700

    [rebase] builder.getSymbolRefAttr to mlir::SymbolRefAttr::get

--- flang/lib/Optimizer/Transforms/ControlFlowConverter.cpp
+++ flang/lib/Optimizer/Transforms/ControlFlowConverter.cpp
@@ -120,7 +120,7 @@ struct SelectTypeOpConversion : public FIROpConversion<SelectTypeOp> {
     // testing if the runtime type of the selector is a derived type or one of
     // that derived type's subtypes.
     auto cmp = rewriter.create<fir::CallOp>(
-        loc, fty, rewriter.getSymbolRefAttr(funName), actuals);
+        loc, fty, mlir::SymbolRefAttr::get(rewriter, funName), actuals);
     auto *thisBlock = rewriter.getInsertionBlock();
     auto *newBlock = createBlock(rewriter, dest);
     rewriter.setInsertionPointToEnd(thisBlock);
