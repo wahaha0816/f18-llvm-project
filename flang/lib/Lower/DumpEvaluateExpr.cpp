2fc67860eb6387bbbe042e64b2a3c56b1d173f95
Eric Schweitz <eschweitz@nvidia.com>
2021-10-26 11:55:37 -0700

    Make filename and class name consistent.

--- flang/lib/Lower/DumpEvaluateExpr.cpp
+++ flang/lib/Lower/DumpEvaluateExpr.cpp
@@ -1,4 +1,4 @@
-//===-- Lower/EvExprDumper.cpp --------------------------------------------===//
+//===-- Lower/DumpEvaluateExpr.cpp ----------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,7 +6,7 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "flang/Lower/EvExprDumper.h"
+#include "flang/Lower/DumpEvaluateExpr.h"
 #include <iostream>
 
 static constexpr char whiteSpacePadding[] =
