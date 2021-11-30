3f882398e00c4f1fe2b0399279ad462c421ed6fc
Valentin Clement (バレンタイン クレメン) <clementval@gmail.com>
2021-11-30 20:53:26 +0100

    Sync after upstreaming builder/runtime #1 (#1282)

--- flang/include/flang/Optimizer/Builder/Runtime/Assign.h
+++ flang/include/flang/Optimizer/Builder/Runtime/Assign.h
@@ -1,4 +1,4 @@
-//===-- Assign.h - generate assignment runtime API calls     ----*- C++ -*-===//
+//===-- Assign.h - generate assignment runtime API calls --------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
