e7ede003b11952c90298a7632e9dbedb1d542175
wahaha0816 <74588321+wahaha0816@users.noreply.github.com>
2021-12-23 12:55:18 +0800

    Update CGJNI.cpp

--- 法兰/库/优化器/代码生成器/CGJNI.cpp
+++ 法兰/库/优化器/代码生成器/CGJNI.cpp
@@ -1,15 +1,4 @@
-//===--------- CGJNI.cpp - Emit LLVM Code for declarations ---------------===//
-//
-//                     The LLVM Compiler Infrastructure
-//
-// This file is distributed under the University of Illinois Open Source
-// License. See LICENSE.TXT for details.
-//
-//===----------------------------------------------------------------------===//
-//
-// This contains code to emit Decl nodes as LLVM code.
-//
-//===----------------------------------------------------------------------===//
+
 
 #include "CGOpenMPRuntimeSpark.h"
 #include "CodeGenFunction.h"
